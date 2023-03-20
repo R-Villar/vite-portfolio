86047e8f4244656ff0a93efc48c1ec414d7b43c8
Remberto Villar <rembertovillar@gmail.com>
2023-03-20 15:20:04 -0400

    styled navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,176 @@
 import { Navbar } from "./Navbar";
+import Box from "@mui/material/Box";
 
 export const Home = () => {
 	return (
-		<div>
-            <Navbar />
+		<Box component='main' sx={{p: 5}}>
+			<Navbar />
 			<h1>My portfolio</h1>
-			<button>Hello button</button>
-		</div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Maecenas varius, ipsum ut vehicula sodales, enim massa
+				condimentum massa, elementum suscipit risus neque non erat.
+				Nulla ultricies lectus id elit tempus porttitor sit amet at
+				tortor. Donec vel enim massa. Phasellus lobortis placerat
+				libero. Cras sed scelerisque lorem. Aenean nec condimentum
+				sapien. Proin vitae arcu eu diam efficitur fermentum. Cras lacus
+				massa, elementum sit amet eros hendrerit, fermentum sagittis
+				odio. Vestibulum a orci arcu. Fusce malesuada metus enim. Fusce
+				at fermentum massa. Vestibulum at scelerisque turpis. Curabitur
+				non augue egestas, bibendum diam nec, malesuada ligula. Vivamus
+				tristique felis sit amet velit auctor eleifend. Proin quis
+				sapien feugiat orci bibendum convallis non id metus. Phasellus
+				lacinia, dolor et eleifend cursus, nunc nibh blandit arcu, et
+				eleifend augue diam et lectus. Etiam non molestie ipsum. Integer
+				lacinia dapibus erat ac sollicitudin. Phasellus feugiat mi non
+				est pharetra, et dignissim orci scelerisque. Etiam sit amet
+				ultricies lacus. Pellentesque et hendrerit turpis, id porttitor
+				orci. Nam eget purus semper, bibendum diam vitae, fermentum
+				mauris. Nunc congue luctus nisi. Morbi commodo porta fringilla.
+				Class aptent taciti sociosqu ad litora torquent per conubia
+				nostra, per inceptos himenaeos. Nunc eu finibus lacus.
+				Pellentesque a dolor eget ante tincidunt congue. Fusce cursus
+				tellus vitae sodales hendrerit. Nulla a velit nec arcu pulvinar
+				blandit. Nulla eu fringilla erat, et euismod felis. Donec
+				imperdiet dolor euismod, eleifend ipsum ut, volutpat justo.
+				Maecenas ultrices, risus ac tincidunt efficitur, arcu mauris
+				rhoncus massa, at euismod orci urna eget orci. Proin consequat
+				nisi ante, vel volutpat mi scelerisque sit amet. Orci varius
+				natoque penatibus et magnis dis parturient montes, nascetur
+				ridiculus mus. In dolor nibh, vestibulum sed tortor vel,
+				bibendum accumsan arcu. Nulla scelerisque eleifend nibh.
+				Suspendisse potenti. Aenean felis ligula, elementum fringilla
+				sodales id, viverra ut dolor. Nulla et justo sed quam
+				consectetur pharetra. Nullam pulvinar, eros at commodo sagittis,
+				felis odio fringilla lectus, lobortis laoreet metus nunc nec
+				sem. Etiam pulvinar, libero eu vehicula eleifend, sem augue
+				tincidunt nulla, at mollis massa enim a lectus. Lorem ipsum
+				dolor sit amet, consectetur adipiscing elit. Donec eu dui et dui
+				efficitur pretium ornare sed nulla. Morbi non tortor non velit
+				laoreet imperdiet pellentesque ac tortor. Sed at ultrices urna.
+				Integer sit amet tincidunt ligula. Aliquam hendrerit dignissim
+				purus in hendrerit. Donec id eros neque. Proin sodales sed velit
+				ac bibendum. Curabitur dictum libero mi, quis tempus ipsum
+				elementum vel. Suspendisse maximus felis ut dolor facilisis
+				blandit. Pellentesque imperdiet nulla et vehicula viverra. Ut
+				scelerisque dapibus porta. Nam egestas quis nibh ut pulvinar.
+				Cras rutrum, dui vel accumsan condimentum, turpis leo auctor
+				orci, dapibus maximus diam massa quis urna. Donec accumsan lacus
+				sit amet tempor pharetra. Fusce ultricies pellentesque enim ut
+				maximus. Nunc ultrices, diam et vulputate hendrerit, mi diam
+				molestie mauris, sed aliquam risus est et purus. Donec quis
+				porta velit, et maximus mi. Cras mauris est, bibendum eget
+				suscipit at, gravida nec urna. Pellentesque non sem eu tellus
+				porttitor scelerisque. Nullam vehicula tortor id odio fermentum
+				scelerisque. Vivamus vitae mollis metus. Integer aliquet dictum
+				orci, sed sollicitudin nisl porttitor ut. Pellentesque viverra,
+				odio eget porta auctor, est est pellentesque felis, sed suscipit
+				ligula ipsum ac mauris. Nullam accumsan velit eget felis
+				pretium, ornare vehicula arcu commodo. Maecenas placerat, lectus
+				at euismod vehicula, ex justo elementum dolor, fermentum
+				convallis tellus quam vitae sem. Donec facilisis dignissim justo
+				nec molestie. Aenean dictum, risus eu dapibus sagittis, ipsum
+				nunc egestas lacus, at blandit eros leo vel arcu. Sed aliquam
+				fermentum justo, non ornare neque mattis at. Fusce laoreet
+				feugiat odio. Donec congue, mauris a porttitor malesuada, nulla
+				risus viverra augue, eget semper sapien tellus id libero. Sed
+				faucibus mollis sapien, a ultrices lorem lacinia eget. Proin
+				volutpat fermentum faucibus. Maecenas mattis magna quis cursus
+				tristique. In ut aliquet dui. Morbi at fringilla orci. Cras
+				tristique posuere risus, vel pretium odio lacinia ut. Sed congue
+				urna odio, eu efficitur metus faucibus vitae. Aenean vel velit
+				tellus. Fusce eu nulla elit. Curabitur aliquam facilisis eros,
+				ut pretium diam feugiat in. Fusce in mi lorem. Etiam vehicula
+				felis ligula, sit amet accumsan ligula pellentesque venenatis.
+				Vestibulum velit nisi, tincidunt in ex maximus, rhoncus blandit
+				purus. Mauris consequat blandit enim, sed auctor nisl lobortis
+				tempor. Aenean id tellus congue, sollicitudin ipsum vel,
+				facilisis sapien. Nunc porta, nisl a fringilla facilisis, diam
+				dolor varius sem, ac elementum dolor tortor placerat erat.
+				Phasellus vitae massa libero. Sed finibus ipsum tristique metus
+				gravida, eget tincidunt justo vulputate. Integer diam justo,
+				viverra eu vehicula sit amet, fermentum ut massa. Proin lobortis
+				lorem sed facilisis consectetur. Nullam enim tortor, rhoncus non
+				ipsum ut, laoreet suscipit odio. Suspendisse potenti. Nunc nulla
+				turpis, sollicitudin eu diam et, mattis sodales tellus. Donec
+				est magna, convallis non fringilla quis, aliquet et nibh. Donec
+				dictum sem eu ligula dictum, tempor fermentum diam mattis. Etiam
+				fermentum nec erat a condimentum. Nullam vulputate finibus
+				lorem, nec interdum tortor porttitor non. Etiam at augue id
+				risus ultricies tempus. Sed suscipit eros ante, sed ultrices
+				libero gravida et. Suspendisse eget nibh vestibulum, iaculis
+				odio in, condimentum eros. Suspendisse potenti. Fusce dictum
+				ligula in mi convallis, a dignissim tellus luctus. Suspendisse
+				eu lacus at felis lobortis venenatis. Morbi pharetra semper
+				mauris sit amet suscipit. Nullam malesuada justo diam, quis
+				dignissim diam varius sit amet. Etiam vestibulum aliquet magna
+				in tincidunt. Praesent in nulla eu nibh pellentesque tempor at
+				ullamcorper ligula. Nunc ut dui metus. Nulla facilisi. Duis
+				feugiat ligula diam, eu vestibulum sem facilisis feugiat. Lorem
+				ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+				varius, ipsum ut vehicula sodales, enim massa condimentum massa,
+				elementum suscipit risus neque non erat. Nulla ultricies lectus
+				id elit tempus porttitor sit amet at tortor. Donec vel enim
+				massa. Phasellus lobortis placerat libero. Cras sed scelerisque
+				lorem. Aenean nec condimentum sapien. Proin vitae arcu eu diam
+				efficitur fermentum. Cras lacus massa, elementum sit amet eros
+				hendrerit, fermentum sagittis odio. Vestibulum a orci arcu.
+				Fusce malesuada metus enim. Fusce at fermentum massa. Vestibulum
+				at scelerisque turpis. Curabitur non augue egestas, bibendum
+				diam nec, malesuada ligula. Vivamus tristique felis sit amet
+				velit auctor eleifend. Proin quis sapien feugiat orci bibendum
+				convallis non id metus. Phasellus lacinia, dolor et eleifend
+				cursus, nunc nibh blandit arcu, et eleifend augue diam et
+				lectus. Etiam non molestie ipsum. Integer lacinia dapibus erat
+				ac sollicitudin. Phasellus feugiat mi non est pharetra, et
+				dignissim orci scelerisque. Etiam sit amet ultricies lacus.
+				Pellentesque et hendrerit turpis, id porttitor orci. Nam eget
+				purus semper, bibendum diam vitae, fermentum mauris. Nunc congue
+				luctus nisi. Morbi commodo porta fringilla. Class aptent taciti
+				sociosqu ad litora torquent per conubia nostra, per inceptos
+				himenaeos. Nunc eu finibus lacus. Pellentesque a dolor eget ante
+				tincidunt congue. Fusce cursus tellus vitae sodales hendrerit.
+				Nulla a velit nec arcu pulvinar blandit. Nulla eu fringilla
+				erat, et euismod felis. Donec imperdiet dolor euismod, eleifend
+				ipsum ut, volutpat justo. Maecenas ultrices, risus ac tincidunt
+				efficitur, arcu mauris rhoncus massa, at euismod orci urna eget
+				orci. Proin consequat nisi ante, vel volutpat mi scelerisque sit
+				amet. Orci varius natoque penatibus et magnis dis parturient
+				montes, nascetur ridiculus mus. In dolor nibh, vestibulum sed
+				tortor vel, bibendum accumsan arcu. Nulla scelerisque eleifend
+				nibh. Suspendisse potenti. Aenean felis ligula, elementum
+				fringilla sodales id, viverra ut dolor. Nulla et justo sed quam
+				consectetur pharetra. Nullam pulvinar, eros at commodo sagittis,
+				felis odio fringilla lectus, lobortis laoreet metus nunc nec
+				sem. Etiam pulvinar, libero eu vehicula eleifend, sem augue
+				tincidunt nulla, at mollis massa enim a lectus. Lorem ipsum
+				dolor sit amet, consectetur adipiscing elit. Donec eu dui et dui
+				efficitur pretium ornare sed nulla. Morbi non tortor non velit
+				laoreet imperdiet pellentesque ac tortor. Sed at ultrices urna.
+				Integer sit amet tincidunt ligula. Aliquam hendrerit dignissim
+				purus in hendrerit. Donec id eros neque. Proin sodales sed velit
+				ac bibendum. Curabitur dictum libero mi, quis tempus ipsum
+				elementum vel. Suspendisse maximus felis ut dolor facilisis
+				blandit. Pellentesque imperdiet nulla et vehicula viverra. Ut
+				scelerisque dapibus porta. Nam egestas quis nibh ut pulvinar.
+				Cras rutrum, dui vel accumsan condimentum, turpis leo auctor
+				orci, dapibus maximus diam massa quis urna. Donec accumsan lacus
+				sit amet tempor pharetra. Fusce ultricies pellentesque enim ut
+				maximus. Nunc ultrices, diam et vulputate hendrerit, mi diam
+				molestie mauris, sed aliquam risus est et purus. Donec quis
+				porta velit, et maximus mi. Cras mauris est, bibendum eget
+				suscipit at, gravida nec urna. Pellentesque non sem eu tellus
+				porttitor scelerisque. Nullam vehicula tortor id odio fermentum
+				scelerisque. Vivamus vitae mollis metus. Integer aliquet dictum
+				orci, sed sollicitudin nisl porttitor ut. Pellentesque viverra,
+				odio eget porta auctor, est est pellentesque felis, sed suscipit
+				ligula ipsum ac mauris. Nullam accumsan velit eget felis
+				pretium, ornare vehicula arcu commodo. Maecenas placerat, lectus
+				at euismod vehicula, ex justo elementum dolor, fermentum
+				convallis tellus quam vitae sem. Donec facilisis dignissim justo
+				nec molestie. Aenean dictum, risus eu dapibus sagittis, ipsum
+				nunc egestas lacus, at blandit eros leo vel arcu. Sed aliquam
+			</p>
+		</Box>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import {useState} from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,40 +6,46 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
 import Fade from "@mui/material/Fade";
-import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+
+
 
 export const Navbar = () => {
+
 	return (
-		<AppBar position='static'>
-			<Stack
-				direction='row'
-				justifyContent='flex-end'
-				alignItems='center'
-				spacing={2}
-			>
-				<Toolbar>
-					<Tooltip
-						title='GitHub profile'
-						TransitionComponent={Fade}
-						TransitionProps={{timeout: 200}}
-					>
-						<IconButton href='https://github.com/R-Villar'>
-							<GitHubIcon fontSize='large' />
-						</IconButton>
-					</Tooltip>
-					<Tooltip
-						title='LinkedIn profile'
-						TransitionComponent={Fade}
-						TransitionProps={{timeout: 200}}
-					>
-						<IconButton href='https://www.linkedin.com/in/remberto-villar/'>
-							<LinkedInIcon fontSize='large' />
-						</IconButton>
-					</Tooltip>
-				</Toolbar>
-			</Stack>
-		</AppBar>
+		<Box sx={{display: "flex"}}>
+			<CssBaseline />
+			<AppBar component='nav' position='fixed'>
+				<Stack
+					direction='row'
+					justifyContent='flex-end'
+					alignItems='center'
+					spacing={2}
+				>
+					<Toolbar>
+						<Tooltip
+							title='GitHub profile'
+							TransitionComponent={Fade}
+							TransitionProps={{timeout: 200}}
+						>
+							<IconButton href='https://github.com/R-Villar'>
+								<GitHubIcon fontSize='large' />
+							</IconButton>
+						</Tooltip>
+						<Tooltip
+							title='LinkedIn profile'
+							TransitionComponent={Fade}
+							TransitionProps={{timeout: 200}}
+						>
+							<IconButton href='https://www.linkedin.com/in/remberto-villar/'>
+								<LinkedInIcon fontSize='large' />
+							</IconButton>
+						</Tooltip>
+					</Toolbar>
+				</Stack>
+			</AppBar>
+		</Box>
 	);
 };
