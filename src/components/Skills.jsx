6efaa86d82dba9009b960f7014588304efc3d90a
Remberto Villar <rembertovillar@gmail.com>
2023-03-22 15:24:20 -0400

    added skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+export const Skills = () => {
+	return (
+		<div>
+			<h1>Skills</h1>
+			<ul>
+				<li>item</li>
+				<li>item</li>
+				<li>item</li>
+				<li>item</li>
+				<li>item</li>
+			</ul>
+		</div>
+	);
+};
