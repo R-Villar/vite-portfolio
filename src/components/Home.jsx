25df3e8e4a7b9cc16073ab01aadac628bc418ccc
Remberto Villar <rembertovillar@gmail.com>
2023-03-25 14:40:07 -0400

    added page icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import {Navbar} from "./Navbar";
 import {About} from "./About";
-import { Welcome } from "./Welcome";
-import { Skills } from "./Skills";
+import {Welcome} from "./Welcome";
+import {Skills} from "./Skills";
 import Box from "@mui/material/Box";
 
 export const Home = () => {
 	return (
 		<Box>
 			<Navbar />
-            <Welcome />
+			<Welcome />
 			<h1>My portfolio</h1>
 			<About />
-            <Skills />
+			<Skills />
 		</Box>
 	);
 };
