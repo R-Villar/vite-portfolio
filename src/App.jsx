38b56f24e68b621ef055c1a0769b94e192ac2b42
Remberto Villar <rembertovillar@gmail.com>
2023-03-27 13:50:30 -0400

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import "./App.css";
-import {Home} from "./components/Home";
-import {Routes, Route} from "react-router-dom";
-
 function App() {
 	return (
 		<div>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
+			<h1>Hello</h1>
 		</div>
 	);
 }
