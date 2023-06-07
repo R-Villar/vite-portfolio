fc2359bfe6be18a9d7b12e589d4505f4957f58ee
Remberto Villar <rembertovillar@gmail.com>
2023-06-07 11:59:31 -0400

    added skills.

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -1,7 +1,8 @@
 import { LineGradient } from "../components/LineGradient";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
-
+import { FaPython, FaGit, FaHtml5, FaReact, FaCss3Alt, FaBootstrap, FaNode } from "react-icons/fa";
+import { SiJavascript, SiRubyonrails, SiRuby, SiMysql, SiMui, SiMongodb, SiFlask } from "react-icons/si";
 export const MySkills = () => {
 	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
@@ -24,12 +25,58 @@ export const MySkills = () => {
 						MY <span className='text-red'>SKILLS</span>
 					</p>
 					<LineGradient width='w-1/3' />
-					<p className='mt-10 mb-7'>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a lorem non mauris auctor
-						auctor. Nullam felis elit,
-					</p>
+					<div className='flex flex-wrap'>
+						<ul className='mt-5 mb-7 w-full'>
+							<li className='flex mt-1'>
+								<FaPython size='2em' /> &nbsp; Python
+							</li>
+							<li className='flex mt-1'>
+								<SiJavascript size='1.8em' /> &nbsp; JavaScript
+							</li>
+							<li className='flex mt-1'>
+								<SiRuby size='2em' /> &nbsp; Ruby
+							</li>
+							<li className='flex mt-1'>
+								<SiRubyonrails size='2em' /> &nbsp; Rails
+							</li>
+							<li className='flex mt-1'>
+								<FaHtml5 size='2em' /> &nbsp; HTML5
+							</li>
+							<li className='flex mt-1'>
+								<FaNode size='2em' /> &nbsp; Node JS
+							</li>
+							<li className='flex mt-1'>
+								<SiFlask size='2em' /> &nbsp; Flask
+							</li>
+						</ul>
+
+						<ul className='mt-5 mb-7 w-full'>
+							<li className='flex mt-1'>
+								<SiMysql size='2em' /> &nbsp; MySQL
+							</li>
+							<li className='flex mt-1'>
+								<SiMongodb size='2em' /> &nbsp; MongoDB
+							</li>
+
+							<li className='flex mt-1'>
+								<FaGit size='2em' /> &nbsp; Git
+							</li>
+							<li className='flex mt-1'>
+								<FaReact size='2em' /> &nbsp; React
+							</li>
+							<li className='flex mt-1'>
+								<FaCss3Alt size='2em' /> &nbsp; CSS
+							</li>
+							<li className='flex mt-1'>
+								<FaBootstrap size='2em' /> &nbsp; Bootstrap
+							</li>
+							<li className='flex mt-1'>
+								<SiMui size='2em' /> &nbsp; Material UI
+							</li>
+						</ul>
+					</div>
 				</motion.div>
-				<div className='mt-16 md:mt-0'>
+				<div className='mt-15 md:mt-0'>
 					{isAboveMediumScreens ? (
 						<div
 							className='relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
@@ -44,7 +91,7 @@ export const MySkills = () => {
 			</div>
 
 			{/* SKILLS */}
-			<div className='md:flex md:justify-between mt-16 gap-32'>
+			<div className='md:flex md:justify-between mt-1 gap-32'>
 				{/* EXPERIENCE */}
 				<motion.div
 					className='md:w-1/3 mt-10'
@@ -68,7 +115,6 @@ export const MySkills = () => {
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum orci ut tellus
 						varius, id eleifend mi blandit. Suspendisse ut mi nec erat vehicula vulputate ac quis
 						ligula. Curabitur leo augue, ultricies nec tortor quis, finibus dapibus risus. Aenean
-						erat quam, ultricies non maximus non, porttitor a lacus
 					</p>
 				</motion.div>
 
@@ -122,7 +168,6 @@ export const MySkills = () => {
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum orci ut tellus
 						varius, id eleifend mi blandit. Suspendisse ut mi nec erat vehicula vulputate ac quis
 						ligula. Curabitur leo augue, ultricies nec tortor quis, finibus dapibus risus. Aenean
-						erat quam, ultricies non maximus non, porttitor a lacus
 					</p>
 				</motion.div>
 			</div>
