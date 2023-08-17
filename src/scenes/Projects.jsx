991e4f0b638f43ac10814c03a34769fdf33525ae
Remberto Villar <rembertovillar@gmail.com>
2023-08-17 19:18:00 -0400

    add projects and desc.

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -42,23 +42,24 @@ export const Projects = ({ setSelectedPage }) => {
 						<div className='md:shrink-0'>
 							<img
 								className='h-48 w-full object-cover md:h-full md:w-48'
-								src='../assets/evon-gym.webp'
-								alt='Project-1'
+								src='../assets/technotes.webp'
+								alt='Modern building architecture'
 							/>
 						</div>
 						<div className='p-8'>
 							<div className='uppercase tracking-wide text-sm text-indigo-500 font-semibold'>
-								Gym app
+								TechNotes
 							</div>
-							<a
-								href='#'
-								className='block mt-1 text-lg leading-tight font-medium text-black hover:underline'
-							>
-								Incredible accommodation for your team
-							</a>
 							<p className='mt-2 text-slate-500'>
-								Looking to take your team away on a retreat to enjoy awesome food and take in
-								some sunshine? We have a list of places to do just that.
+								A Fullstack web application for a repair shop. In the application we have
+								different roles ranging from employees, managers and admins. anyone regardless
+								of their role can create notes, and complete them. Employees can view and edit
+								the notes but only managers or admins can delete the notes. Manager and admins
+								are the only ones that can access the user settings to both create new user or
+								delete them.
+							</p>
+							<p className='block mt-1 text-md leading-tight font-medium text-black'>
+								MongoDB, Express.js, Node.js, React, Redux
 							</p>
 							<div className='flex flex-row gap-x-2.5 py-2'>
 								<a
@@ -68,36 +69,34 @@ export const Projects = ({ setSelectedPage }) => {
 								>
 									<FaGithub title='go to site' size='1.5rem' color='black' />
 								</a>
-								<a>
+								<a href='https://notes-t5qx.onrender.com/' target='_blank' rel='noreferrer'>
 									<BiLinkExternal title='go to site' size='1.5rem' color='black' />
 								</a>
 							</div>
 						</div>
 					</div>
 				</div>
-
+				{/* ROW 2 */}
 				<div className='max-w-md mx-auto bg-project-card rounded-lg shadow-md overflow-hidden md:max-w-2xl'>
 					<div className='md:flex'>
 						<div className='md:shrink-0'>
 							<img
 								className='h-48 w-full object-cover md:h-full md:w-48'
-								src='../assets/project-1.jpeg'
-								alt='Modern building architecture'
+								src='../assets/evon-gym.webp'
+								alt='Project-1'
 							/>
 						</div>
 						<div className='p-8'>
 							<div className='uppercase tracking-wide text-sm text-indigo-500 font-semibold'>
-								Company retreats
+								EVoGym
 							</div>
-							<a
-								href='#'
-								className='block mt-1 text-lg leading-tight font-medium text-black hover:underline'
-							>
-								Incredible accommodation for your team
-							</a>
 							<p className='mt-2 text-slate-500'>
-								Looking to take your team away on a retreat to enjoy awesome food and take in
-								some sunshine? We have a list of places to do just that.
+								A complete front-end only application. This application was created with
+								React, Typescript and Framer Motion. The application if fully responsive with
+								both desktop and mobile view.
+							</p>
+							<p className='block mt-1 text-md leading-tight font-medium text-black'>
+								React, Typescript, TailwindCSS
 							</p>
 							<div className='flex flex-row gap-x-2.5 py-2'>
 								<a
@@ -107,7 +106,7 @@ export const Projects = ({ setSelectedPage }) => {
 								>
 									<FaGithub title='go to site' size='1.5rem' color='black' />
 								</a>
-								<a>
+								<a href='https://evogym-one.vercel.app/' target='_blank' rel='noreferrer'>
 									<BiLinkExternal title='go to site' size='1.5rem' color='black' />
 								</a>
 							</div>
